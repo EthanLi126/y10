d09071925c1a7b60e72248b0e26d9ba732d1248b
Ethan li <ethan.li@NotARandomStringOfNumbers.local>
2019-09-19 21:36:43 -0400

    website and css created for api

--- api ethan/api.py
+++ api ethan/api.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 # test for internet connection
 def is_internet():
         try:
-            urlopen('https://www.google.com', timeout=1)
+            urlopen('https://www.google.com', timeout=3)
             return True
         except urllib.error.URLError as Error:
                     return False
@@ -13,17 +13,46 @@ def is_internet():
 # if internet is connected
 if is_internet():
         
-    def writeHTML(state,date,temp,mintemp,maxtemp,windspeed,humidity):
+    def writeHTML(state,img,date,temp,mintemp,maxtemp,windspeed):
             myfile = open("Weather.html","w")
-            myfile.write("<head>")
-            myfile.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">")
-            myfile.write("</head>")
-            myfile.write("<body>")
-            myfile.write("<div class=\"pagecontainer\">")
-            myfile.write(state)
+            myfile.write("<head>\n")
+            myfile.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n")
+            myfile.write("</head>\n")
+            myfile.write("<body>\n")
+            myfile.write("<div class=\"pagecontainer\">\n")
+            myfile.write("  <div class=\"header\">\n")
+            myfile.write("      <h1>Toronto Weather</h1>\n")
+            myfile.write("  </div>\n")
+            myfile.write("  <div class = \"weather\">\n")
+            myfile.write("      <h5>")
             myfile.write(date)
-            myfile.write("</div>")
-            myfile.write("</body>")
+            myfile.write("</h5>\n")
+            myfile.write("      <img src=\"")
+            myfile.write(img)
+            myfile.write("\" style=\"width:10%\">\n")
+            myfile.write("      <h4>")
+            myfile.write(state)
+            myfile.write("</h4>\n")
+            myfile.write("      <h2>")
+            myfile.write(str(temp))
+            myfile.write("°C</h2>\n")
+            myfile.write("  </div>\n")
+            myfile.write("  <div class = \"weather2\">\n")
+            myfile.write("      <h4>")
+            myfile.write("Low of: ")
+            myfile.write(str(mintemp))
+            myfile.write("°C</h4>\n")
+            myfile.write("      <h4>")
+            myfile.write("High of: ")
+            myfile.write(str(maxtemp))
+            myfile.write("°C</h4>\n")
+            myfile.write("      <h4>")
+            myfile.write("Windspeed: ")
+            myfile.write(str(windspeed))
+            myfile.write(" mph</h4>\n")
+            myfile.write("  </div>\n")
+            myfile.write("</div>\n")
+            myfile.write("</body>\n")
             myfile.close()
 
     def main():
@@ -34,18 +63,41 @@ if is_internet():
             if (response.status_code == 200):
                             wjson = response.json()
                             state = (wjson["consolidated_weather"][0]['weather_state_name'])
+                            abbr = (wjson["consolidated_weather"][0]['weather_state_abbr'])
                             date = (wjson["consolidated_weather"][0]['applicable_date'])
                             temp = (wjson["consolidated_weather"][0]['the_temp'])
                             mintemp = (wjson["consolidated_weather"][0]['min_temp'])
                             maxtemp = (wjson["consolidated_weather"][0]['max_temp'])
-                            windspeed = (wjson["consolidated_weather"][0]['wind_speed'])
-                            humidity = (wjson["consolidated_weather"][0]['humidity'])
-                            writeHTML(state,date,temp,mintemp,maxtemp,windspeed,humidity) 
+                            windspeedi = (wjson["consolidated_weather"][0]['wind_speed'])
 
-                            
-                            
-                            print(state, temp, mintemp, maxtemp, windspeed, humidity)
-                            
+                            windspeed = round(windspeedi)
+
+                            if(abbr == "lc"):
+                                img="images/lc.svg"
+                            elif(abbr == "c"):
+                                img="images/c.svg"
+                            elif(abbr == "h"):
+                                img="images/h.svg"
+                            elif(abbr == "hc"):
+                                img="images/hc.svg"
+                            elif(abbr == "hr"):
+                                img="images/hr.svg"
+                            elif(abbr == "lr"):
+                                img="images/lr.svg"
+                            elif(abbr == "s"):
+                                img="images/s.svg"
+                            elif(abbr == "sl"):
+                                img="images/sl.svg"
+                            elif(abbr == "sn"):
+                                img="images/sn.svg"
+                            elif(abbr == "t"):
+                                img="images/t.svg"
+
+                    
+                                
+                                    
+                            writeHTML(state,img,date,temp,mintemp,maxtemp,windspeed) 
+                            #print(state, temp, mintemp, maxtemp, windspeed,)
                             print("HTML file has been written. Please proceed")
     
                             
@@ -59,6 +111,6 @@ if is_internet():
 
 
 else:
-    print("Internet disconnected. Please connect to the internet before running the program")
+    print("Internet disconnected. Please connect to the internet before running the program.")
     
 
